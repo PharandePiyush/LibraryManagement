8598a0e3e5bbe93dd97700a5275d67a16b8df96d
Piyush Pharande <pharandepiyush06@gmail.com>
2026-05-11 15:36:08 +0530

    Commit and Push

--- LibraryManagement/src/main/java/com/example/demo/Entity/User.java
+++ LibraryManagement/src/main/java/com/example/demo/Entity/User.java
@@ -32,17 +32,15 @@ public class User {
     @NotBlank(message = "Full name is required")
     private String fullName;
     
-    // One User can have many Books
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Book> books = new ArrayList<>();
     
-    // Helper method to add book
     public void addBook(Book book) {
         books.add(book);
         book.setUser(this);
     }
     
-    // Helper method to remove book
+    
     public void removeBook(Book book) {
         books.remove(book);
         book.setUser(null);
